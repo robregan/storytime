26952d4734a16aaabf3c3b160edfb79bcfac6f3d
rob <robert_regan86@yahoo.com>
2021-04-09 14:14:51 -0400

    storybook app

---
+++ middleware/auth.js
@@ -0,0 +1,34 @@
+// module.exports = {
+//     ensureAuth: function (req, res, next){
+//         if(req.isAuthenticated()){
+//             return next()
+//         } else {
+//             res.redirect('/')
+//         }
+//     },
+//     ensureGuest: function (req, res, next) {
+//         if(req.isAuthenticated()){
+//             res.redirect('/dashboard')
+//         } else{
+//             return next()
+//         }
+//     }
+// }
+
+module.exports = {
+    ensureAuth: function (req, res, next) {
+      if (req.isAuthenticated()) {
+        return next()
+      } else {
+        res.redirect('/')
+      }
+    },
+    ensureGuest: function (req, res, next) {
+      if (!req.isAuthenticated()) {
+        return next();
+      } else {
+        res.redirect('/dashboard');
+      }
+    },
+  }
+  
