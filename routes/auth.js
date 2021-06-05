7eba5de4737a70857d108f970497e04c09f67cce
rob <robert_regan86@yahoo.com>
2021-06-04 21:49:28 -0400

    sdf

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,9 @@
 const express = require('express')
 const passport = require('passport')
 const router = express.Router()
-const app = express()
 
-if(process.env.NODE_ENV === 'production') {
-    app.use((req, res, next) => {
-      if (req.header('x-forwarded-proto') !== 'https')
-        res.redirect(`https://${req.header('host')}${req.url}`)
-      else
-        next()
-    })
-  }
+
+
 // auth with google
 // GET /auth/google
 
