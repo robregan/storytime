827b1b0a7eb8def1f85c3e7f5bcd14ac93499a64
rob <robert_regan86@yahoo.com>
2021-06-04 21:47:33 -0400

    sd

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const passport = require('passport')
 const router = express.Router()
+const app = express()
 
 if(process.env.NODE_ENV === 'production') {
     app.use((req, res, next) => {
